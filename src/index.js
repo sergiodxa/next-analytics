734715ced88587c7fa99ac21f93f9788d0e6f0ce
Joe Cohen <joecohenr@gmail.com>
2018-08-06 13:27:45 -0500

    remove router as a dep

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import Router from 'next/router'
 import withGA from "next-ga";
 import withFBQ from "next-fbq";
 
-export default ({ ga, fbq } = {}) => Page => {
+export default (router, { ga, fbq } = {}) => Page => {
   const hocs = [];
 
   // add withGA (passing GA code) to list of HOCs
-  if (ga) hocs.push(withGA(ga, Router));
+  if (ga) hocs.push(withGA(ga, router));
   
   // add withFBQ (passing FBQ code) to list of HOCs
-  if (fbq) hocs.push(withFBQ(fbq, Router));
+  if (fbq) hocs.push(withFBQ(fbq, router));
 
   // apply each HOC to the Page
   return hocs.reduce((WrappedPage, hoc) => hoc(WrappedPage), Page);
